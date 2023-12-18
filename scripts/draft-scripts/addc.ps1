289350b5bb198f612c585fbb01aeb2014e6c2400
mikes <mikes@cyberrange>
2023-12-18 15:08:03 -0800

    change addc

--- scripts/draft-scripts/addc.ps1
+++ scripts/draft-scripts/addc.ps1
@@ -1,3 +1,6 @@
+
+Add-WindowsFeature RSAT-AD-PowerShell
+
 # Install Active Directory Domain Services
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 
@@ -8,14 +11,3 @@ Install-ADDSForest `
     -ForestMode Win2012R2 `
     -SafeModeAdministratorPassword (ConvertTo-SecureString -AsPlainText "YourPassword" -Force) `
     -Force:$true
-
-# Create Organizational Units (OUs)
-New-ADOrganizationalUnit -Name "Sales" -Path "DC=YourDomainName,DC=com"
-New-ADOrganizationalUnit -Name "Marketing" -Path "DC=YourDomainName,DC=com"
-
-# Create Users
-$ouPath = "OU=Sales,DC=YourDomainName,DC=com"
-New-ADUser -SamAccountName "User1" -UserPrincipalName "User1@YourDomainName.com" -Name "User1" -GivenName "User" -Surname "One" -Enabled $true -Path $ouPath -AccountPassword (ConvertTo-SecureString -AsPlainText "User1Password" -Force) -PassThru
-
-$ouPath = "OU=Marketing,DC=YourDomainName,DC=com"
-New-ADUser -SamAccountName "User2" -UserPrincipalName "User2@YourDomainName.com" -Name "User2" -GivenName "User" -Surname "Two" -Enabled $true -Path $ouPath -AccountPassword (ConvertTo-SecureString -AsPlainText "User2Password" -Force) -PassThru
