830d25484ce6ff54994df7f14ccc203d4fec1331
mikes <mikes@cyberrange>
2023-12-18 14:19:28 -0800

    trying a new one

---
+++ scripts/draft-scripts/add2.ps1
@@ -0,0 +1,17 @@
+# Prompt for static IPv4 Address
+$ipv4Address = Read-Host "Enter the static IPv4 address (e.g., 192.168.1.100)"
+
+# Prompt for DNS server
+$dnsServer = Read-Host "Enter the DNS server address (e.g., 8.8.8.8)"
+
+# Prompt for new server name
+$newServerName = Read-Host "Enter the new server name"
+
+# Set static IPv4 address
+New-NetIPAddress -InterfaceAlias "Ethernet" -IPAddress $ipv4Address -PrefixLength 24 -DefaultGateway "192.168.1.1"
+
+# Set DNS server
+Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses $dnsServer
+
+# Rename the Windows Server
+Rename-Computer -NewName $newServerName -Force -Restart
