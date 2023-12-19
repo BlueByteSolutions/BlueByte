7cd0120e178e329f33615eb00bc2e737c8ed6f4d
mikes <mikes@cyberrange>
2023-12-18 18:08:38 -0800

    addc change

--- scripts/draft-scripts/addc.ps1
+++ scripts/draft-scripts/addc.ps1
@@ -1,46 +1,13 @@
-# Define variables
-$domainAdminUsername = "Administrator"
-$domainAdminPassword = "YourPassword"
-$domainName = "YourDomain.local"
-$domainNetBIOSName = "YourDomain"
-$domainAdminPasswordSecure = ConvertTo-SecureString $domainAdminPassword -AsPlainText -Force
 
-# Set the execution policy (if needed)
-# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+Add-WindowsFeature RSAT-AD-PowerShell
 
-# Install AD DS role
-try {
-    Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -ErrorAction Stop
-    Write-Host "AD DS role installed successfully."
-} catch {
-    Write-Host "Error installing AD DS role: $_"
-    exit 1
-}
+# Install Active Directory Domain Services
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 
-# Promote to domain controller
-try {
-    Install-ADDSDomainController -NoGlobalCatalog:$false `
-        -CreateDnsDelegation:$false -CriticalReplicationOnly:$false -DatabasePath "C:\Windows\NTDS" `
-        -DomainName $domainName -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$false `
-        -SiteName "Default-First-Site-Name" -SysvolPath "C:\Windows\SYSVOL" -Force:$true `
-        -Credential (New-Object System.Management.Automation.PSCredential ($domainAdminUsername, $domainAdminPasswordSecure)) -ErrorAction Stop
-
-    Write-Host "Domain controller promotion completed successfully."
-} catch {
-    Write-Host "Error promoting to domain controller: $_"
-    exit 1
-}
-
-# Restart the server
-try {
-    Restart-Computer -Force -ErrorAction Stop
-    Write-Host "Server restarted successfully."
-} catch {
-    Write-Host "Error restarting the server: $_"
-    exit 1
-}
-
-# Script completed successfully
-Write-Host "Script executed successfully."
-exit 0
-
+# Promote to Domain Controller and create a new forest
+Install-ADDSForest `
+    -DomainName "YourDomainName.com" `
+    -DomainMode Win2012R2 `
+    -ForestMode Win2012R2 `
+    -SafeModeAdministratorPassword (ConvertTo-SecureString -AsPlainText "YourPassword" -Force) `
+    -Force:$true
