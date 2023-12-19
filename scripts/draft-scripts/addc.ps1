161a82df0a39d6bc61f742bba7a217ddf2905f14
mikes <mikes@cyberrange>
2023-12-19 14:16:00 -0800

    addc change

--- scripts/draft-scripts/addc.ps1
+++ scripts/draft-scripts/addc.ps1
@@ -3,15 +3,21 @@
 # Date of latest revision:      12/18/2023
 # Purpose:                     creates new domain and forest
 
-Add-WindowsFeature RSAT-AD-PowerShell
+# Prompt the user for input
+$domainName = Read-Host -Prompt "Enter the domain name"
+$netBIOSName = Read-Host -Prompt "Enter the NetBIOS name"
 
-# Install Active Directory Domain Services
+# Install the required Windows features
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 
-# Promote to Domain Controller and create a new forest
+# Promote the server to a domain controller and configure Active Directory Forest
 Install-ADDSForest `
-    -DomainName "YourDomainName.com" `
-    -DomainMode Win2012R2 `
-    -ForestMode Win2012R2 `
-    -SafeModeAdministratorPassword (ConvertTo-SecureString -AsPlainText "YourPassword" -Force) `
+    -DomainName $domainName `
+    -DomainMode "WinThreshold" `
+    -ForestMode "WinThreshold" `
+    -DomainNetbiosName $netBIOSName `
+    -InstallDns:$true `
     -Force:$true
+
+# Restart the server to complete the installation
+Restart-Computer -Force
