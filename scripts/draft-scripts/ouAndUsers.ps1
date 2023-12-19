2124678988ea4ca5c1c37af90dd9c32139921f87
mikes <mikes@cyberrange>
2023-12-18 16:28:12 -0800

    adding to ouandusers

--- scripts/draft-scripts/ouAndUsers.ps1
+++ scripts/draft-scripts/ouAndUsers.ps1
@@ -1,43 +1,59 @@
+# Import the Active Directory module
+Import-Module ActiveDirectory
+
 # Function to create an Organizational Unit
 function Create-OU {
     param (
-        [string]$OUName
+        [string]$ouName
     )
 
-    # Construct the LDAP path for the new OU
-    $OUPath = "OU=$OUName,DC=corp,DC=BlueByte,DC=com" # Update with your actual domain information
-
-    # Create the new OU
-    New-ADOrganizationalUnit -Name $OUName -Path "OU=YourParentOU,DC=corp,DC=BlueByte,DC=com" -Description "Description for $OUName"
+    try {
+        New-ADOrganizationalUnit -Name $ouName -Path "OU=Users,DC=corp,DC=BlueByte,DC=com" -ErrorAction Stop
+        Write-Host "Organizational Unit '$ouName' created successfully." -ForegroundColor Green
+    } catch {
+        Write-Host "Error creating Organizational Unit: $_" -ForegroundColor Red
+    }
 }
 
 # Function to create a User
 function Create-User {
     param (
-        [string]$Username,
-        [string]$Password,
-        [string]$Description
+        [string]$firstName,
+        [string]$lastName,
+        [string]$userName,
+        [string]$password
     )
 
-    # Construct the LDAP path for the new user
-    $UserPath = "OU=YourOU,DC=corp,DC=BlueByte,DC=com" # Update with the OU path where you want to create the user
+    try {
+        $userParams = @{
+            SamAccountName  = $userName
+            GivenName       = $firstName
+            Surname         = $lastName
+            UserPrincipalName = "$userName@corp.BlueByte.com"
+            Name            = "$firstName $lastName"
+            DisplayName     = "$firstName $lastName"
+            Enabled         = $true
+            Path            = "OU=Users,DC=corp,DC=BlueByte,DC=com"
+            AccountPassword = (ConvertTo-SecureString -AsPlainText $password -Force)
+            ChangePasswordAtLogon = $true
+        }
 
-    # Create the new user
-    New-ADUser -SamAccountName $Username -UserPrincipalName "$Username@corp,DC=BlueByte.com" -Name $Username -GivenName $Username -Surname "User" -Description $Description -AccountPassword (ConvertTo-SecureString -AsPlainText $Password -Force) -Enabled $true -Path $UserPath
+        New-ADUser @userParams -ErrorAction Stop
+        Write-Host "User '$userName' created successfully." -ForegroundColor Green
+    } catch {
+        Write-Host "Error creating user: $_" -ForegroundColor Red
+    }
 }
 
-# Main script
+# Get user input for Organizational Unit
+$ouName = Read-Host "Enter the name of the Organizational Unit"
 
-# Prompt for OU name
-$OUName = Read-Host "Enter the name of the Organizational Unit (OU)"
+# Get user input for User
+$firstName = Read-Host "Enter the first name of the user"
+$lastName = Read-Host "Enter the last name of the user"
+$userName = Read-Host "Enter the username for the user"
+$password = Read-Host "Enter the password for the user" -AsSecureString
 
-# Create the OU
-Create-OU -OUName $OUName
-
-# Prompt for User details
-$Username = Read-Host "Enter the username"
-$Password = Read-Host -AsSecureString "Enter the password"
-$Description = Read-Host "Enter the user description"
-
-# Create the User
-Create-User -Username $Username -Password $Password -Description $Description
+# Call functions to create OU and User
+Create-OU -ouName $ouName
+Create-User -firstName $firstName -lastName $lastName -userName $userName -password $password
