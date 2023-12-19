964a493dd6b4397a9f187a13462416f39f349abc
mikes <mikes@cyberrange>
2023-12-19 11:35:37 -0800

    report changes

--- scripts/draft-scripts/winReport.ps1
+++ scripts/draft-scripts/winReport.ps1
@@ -23,7 +23,7 @@ $EmailParameters = @{
 }
 $FormattingParameters = @{
     CompanyBranding        = @{
-        Logo   = 'bluebyte.png'
+        Logo   = '/home/mikes/project301/BlueByte/scripts/draft-scripts/bluebyte.png'
         Width  = '200'
         Height = ''
         Link   = 'https://github.com/BlueByteSolutions/BlueByte'
