d44b9d5574f0fb1e06ceac71be3b7b88c4336260
mikes <mikes@cyberrange>
2023-12-19 17:14:21 -0800

    change to temp passwords

--- scripts/draft-scripts/ouUsersAuto.ps1
+++ scripts/draft-scripts/ouUsersAuto.ps1
@@ -26,7 +26,7 @@ function Create-User {
     )
 
     try {
-        $userName = ($firstName[0] + "." + $lastName).ToLower()
+        $userName = ($firstName[0].ToUpper() + "." + $lastName).ToLower()
         $userParams = @{
             SamAccountName    = $userName
             GivenName         = $firstName
@@ -53,10 +53,10 @@ foreach ($ou in $executiveTeamOUs) {
     Create-OU -ouName $ou
 }
 
-Create-User -ouName "CEO" -firstName "Martin" -lastName "Brody" -password "jmartintemppass1"
-Create-User -ouName "CFO" -firstName "Larry" -lastName "Vaughn" -password "lvaughntemppass1"
-Create-User -ouName "CTO" -firstName "Matt" -lastName "Hooper" -password "mhoopertemppass1"
-Create-User -ouName "COO" -firstName "Quint" -lastName "" -password "qquinttemppass1"
+Create-User -ouName "CEO" -firstName "Martin" -lastName "Brody" -password "Jmartintemppass1"
+Create-User -ouName "CFO" -firstName "Larry" -lastName "Vaughn" -password "Lvaughntemppass1"
+Create-User -ouName "CTO" -firstName "Matt" -lastName "Hooper" -password "Mhoopertemppass1"
+Create-User -ouName "COO" -firstName "Quint" -lastName "" -password "Qquinttemppass1"
 
 # Create Sales Team OUs and Users
 $salesTeamOUs = @("SalesManager", "MarketingSpecialist", "AccountExecutive")
@@ -64,9 +64,9 @@ foreach ($ou in $salesTeamOUs) {
     Create-OU -ouName $ou
 }
 
-Create-User -ouName "SalesManager" -firstName "Ellen" -lastName "Brody" -password "ebrodytemppass1"
-Create-User -ouName "MarketingSpecialist" -firstName "Matt" -lastName "HooperJr" -password "mhooperjrtemppass1"
-Create-User -ouName "AccountExecutive" -firstName "Harry" -lastName "Meadows" -password "hmeadowstemppass1"
+Create-User -ouName "SalesManager" -firstName "Ellen" -lastName "Brody" -password "Ebrodytemppass1"
+Create-User -ouName "MarketingSpecialist" -firstName "Matt" -lastName "HooperJr" -password "Mhooperjrtemppass1"
+Create-User -ouName "AccountExecutive" -firstName "Harry" -lastName "Meadows" -password "Hmeadowstemppass1"
 
 # Create R&D OUs and Users
 $rdOUs = @("RDManager", "SoftwareEngineer", "DataScientist", "UXUIDesigner")
@@ -74,10 +74,10 @@ foreach ($ou in $rdOUs) {
     Create-OU -ouName $ou
 }
 
-Create-User -ouName "RDManager" -firstName "Dr." -lastName "Elkins" -password "delkinstemppass1"
-Create-User -ouName "SoftwareEngineer" -firstName "Mike" -lastName "Brody" -password "mbrodytemppass1"
-Create-User -ouName "DataScientist" -firstName "Sean" -lastName "Brody" -password "sbrodytemppass1"
-Create-User -ouName "UXUIDesigner" -firstName "Tina" -lastName "Wilcox" -password "twilcoxtemppass1"
+Create-User -ouName "RDManager" -firstName "Dr." -lastName "Elkins" -password "Delkinstemppass1"
+Create-User -ouName "SoftwareEngineer" -firstName "Mike" -lastName "Brody" -password "Mbrodytemppass1"
+Create-User -ouName "DataScientist" -firstName "Sean" -lastName "Brody" -password "Sbrodytemppass1"
+Create-User -ouName "UXUIDesigner" -firstName "Tina" -lastName "Wilcox" -password "Twilcoxtemppass1"
 
 # Create IT Management OUs and Users
 $itOUs = @("ITDirector", "NetworkAdministrator", "CybersecuritySpecialist", "SystemAdministrator")
@@ -85,10 +85,10 @@ foreach ($ou in $itOUs) {
     Create-OU -ouName $ou
 }
 
-Create-User -ouName "ITDirector" -firstName "Leonard" -lastName "Hendricks" -password "lhendrickstemppass1"
-Create-User -ouName "NetworkAdministrator" -firstName "Larry" -lastName "VaughnJr" -password "lvaughnjrtemppass1"
-Create-User -ouName "CybersecuritySpecialist" -firstName "Carl" -lastName "Gottlieb" -password "cgottliebtemppass1"
-Create-User -ouName "SystemAdministrator" -firstName "Josh" -lastName "Mills" -password "jmillsstemppass1"
+Create-User -ouName "ITDirector" -firstName "Leonard" -lastName "Hendricks" -password "Lhendrickstemppass1"
+Create-User -ouName "NetworkAdministrator" -firstName "Larry" -lastName "VaughnJr" -password "Lvaughnjrtemppass1"
+Create-User -ouName "CybersecuritySpecialist" -firstName "Carl" -lastName "Gottlieb" -password "Cgottliebtemppass1"
+Create-User -ouName "SystemAdministrator" -firstName "Josh" -lastName "Mills" -password "Jmillsstemppass1"
 
 # Create HR Management OUs and Users
 $hrOUs = @("HRManager", "RecruitmentSpecialist", "EmployeeRelationsSpecialist", "TrainingCoordinator")
@@ -96,7 +96,7 @@ foreach ($ou in $hrOUs) {
     Create-OU -ouName $ou
 }
 
-Create-User -ouName "HRManager" -firstName "Lorraine" -lastName "Kitner" -password "lkitnertemppass1"
-Create-User -ouName "RecruitmentSpecialist" -firstName "Sarah" -lastName "Thompson" -password "sthompsontemppass1"
-Create-User -ouName "EmployeeRelationsSpecialist" -firstName "Robert" -lastName "Kintner" -password "rkintnertemppass1"
-Create-User -ouName "TrainingCoordinator" -firstName "Amanda" -lastName "Mills" -password "amillstemppass1"
+Create-User -ouName "HRManager" -firstName "Lorraine" -lastName "Kitner" -password "Lkitnertemppass1"
+Create-User -ouName "RecruitmentSpecialist" -firstName "Sarah" -lastName "Thompson" -password "Sthompsontemppass1"
+Create-User -ouName "EmployeeRelationsSpecialist" -firstName "Robert" -lastName "Kintner" -password "Rkintnertemppass1"
+Create-User -ouName "TrainingCoordinator" -firstName "Amanda" -lastName "Mills" -password "Amillstemppass1"
