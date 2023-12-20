a7cb733781dcf0550b3d7a64bf88125c428b2f74
mikes <mikes@cyberrange>
2023-12-19 17:38:05 -0800

    change to temp passwords

--- scripts/draft-scripts/ouUsersAuto.ps1
+++ scripts/draft-scripts/ouUsersAuto.ps1
@@ -22,71 +22,72 @@ function Create-User {
         [string]$ouName,
         [string]$firstName,
         [string]$lastName,
-        [string]$role
+        [string]$title
     )
 
     try {
-        $userName = "$($firstName[0]).$lastName"
-        $password = "Strongpass1"
+        $username = "$($firstName[0]).$lastName"
+        $password = ConvertTo-SecureString -String "Strongpass1" -AsPlainText -Force
 
         $userParams = @{
-            SamAccountName  = $userName
+            SamAccountName  = $username
             GivenName       = $firstName
             Surname         = $lastName
-            UserPrincipalName = "$userName@corp.BlueByte.com"
+            UserPrincipalName = "$username@corp.BlueByte.com"
             Name            = "$firstName $lastName"
-            DisplayName     = "$firstName $lastName"
+            DisplayName     = "$title $firstName $lastName"
             Enabled         = $true
             Path            = "OU=$ouName,DC=corp,DC=BlueByte,DC=com"
-            AccountPassword = (ConvertTo-SecureString -AsPlainText $password -Force)
+            AccountPassword = $password
             ChangePasswordAtLogon = $true
         }
 
         New-ADUser @userParams -ErrorAction Stop
-        Write-Host "User '$userName' created successfully." -ForegroundColor Green
+        Write-Host "User '$username' created successfully." -ForegroundColor Green
     } catch {
         Write-Host "Error creating user: $_" -ForegroundColor Red
     }
 }
 
-# Define OUs and users
+# Define OUs and Users
 $ouList = @(
     "Executive Team",
     "Sales Team",
     "R&D",
-    "IT Management",
+    "IT",
     "HR"
 )
 
 $userList = @(
-    ("Martin", "Brody", "CEO"),
-    ("Larry", "Vaughn", "CFO"),
-    ("Matt", "Hooper", "CTO"),
-    ("Quint", "", "COO"),
-    ("Ellen", "Brody", "Sales Manager"),
-    ("Matt", "Hooper Jr.", "Marketing Specialist"),
-    ("Harry", "Meadows", "Account Executive"),
-    ("Chrissie", "Watkins", "Social Media Coordinator"),
-    ("Dr.", "Elkins", "R&D Manager"),
-    ("Mike", "Brody", "Software Engineer"),
-    ("Sean", "Brody", "Data Scientist"),
-    ("Tina", "Wilcox", "UX/UI Designer"),
-    ("Leonard", "Hendricks", "IT Director"),
-    ("Larry", "Vaughn Jr.", "Network Administrator"),
-    ("Carl", "Gottlieb", "Cybersecurity Specialist"),
-    ("Josh", "Mills", "System Administrator"),
-    ("Lorraine", "Kitner", "HR Manager"),
-    ("Sarah", "Thompson", "Recruitment Specialist"),
-    ("Robert", "Kintner", "Employee Relations Specialist"),
-    ("Amanda", "Mills", "Training Coordinator")
+    ("CEO", "Martin", "Brody"),
+    ("CFO", "Larry", "Vaughn"),
+    ("CTO", "Matt", "Hooper"),
+    ("COO", "Quint", "William"),
+    ("Sales Manager", "Ellen", "Brody"),
+    ("Marketing Specialist", "Matt", "Hooper Jr."),
+    ("Account Executive", "Harry", "Meadows"),
+    ("Social Media Coordinator", "Chrissie", "Watkins"),
+    ("R&D Manager", "Drake", "Elkins"),
+    ("Software Engineer", "Mike", "Brody"),
+    ("Data Scientist", "Sean", "Brody"),
+    ("UX/UI Designer", "Tina", "Wilcox"),
+    ("IT Director", "Leonard", "Hendricks"),
+    ("Network Administrator", "Larry", "Vaughn Jr."),
+    ("Cybersecurity Specialist", "Carl", "Gottlieb"),
+    ("System Administrator", "Josh", "Mills"),
+    ("HR Manager", "Lorraine", "Kitner"),
+    ("Recruitment Specialist", "Sarah", "Thompson"),
+    ("Employee Relations Specialist", "Robert", "Kintner"),
+    ("Training Coordinator", "Amanda", "Mills")
 )
 
-# Call functions to create OUs and Users
+# Create OUs
 foreach ($ou in $ouList) {
     Create-OU -ouName $ou
 }
 
-for ($i = 0; $i -lt $userList.Count; $i++) {
-    $user = $userList[$i]
-    Create-User -ouName $ouList[$i] -firstName $user[0] -lastName $user[1] -role $user[2]
+# Create Users
+foreach ($user in $userList) {
+    $ouName = $user[0].Split(' ')[-1]  # Extract department from the title
+    Create-User -ouName $ouName -firstName $user[1] -lastName $user[2] -title $user[0]
 }
